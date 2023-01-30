4a5e844e239cc6364ef23f8c6db67a48d7331453
Olha Rakytianska <olya.rakytyanska@gmail.com>
2023-01-30 17:54:02 +0200

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function createGalleryBox(items) {
+  return items.map(item => {
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery__item');
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.href = item.original;
+
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.src = item.preview;
+    galleryImage.dataset.source = item.original;
+    galleryImage.alt = item.description;
+
+    galleryLink.append(galleryImage);
+    galleryItem.append(galleryLink);
+
+    return galleryItem;
+  });
+}
+
+const galleryContainerElements = createGalleryBox(galleryItems);
+galleryContainer.append(...galleryContainerElements);
+
+galleryContainer.addEventListener('click', onModalOpen);
+
+function onModalOpen(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src='${event.target.dataset.source}'>
+    </div>
+`);
+
+  window.addEventListener('keydown', onEscKeyPress);
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+
+  instance.show();
+}
